d70bc09d22887ad0cb8459e8d4b51664fc730d15
Daniel Murphy <danielmurphycs@gmail.com>
2021-11-17 15:58:44 -0800

    added homescreen base

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -11,13 +13,15 @@ function HomeScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
-
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'My home' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
