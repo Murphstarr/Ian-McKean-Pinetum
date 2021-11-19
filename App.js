c787bd565b08d67d73d50b925af846899e4a2bc2
Daniel Murphy <danielmurphycs@gmail.com>
2021-11-18 21:10:22 -0800

    added expo to project

--- App.js
+++ App.js
@@ -1,39 +1,82 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Stack = createNativeStackNavigator();
-
-function HomeScreen() {
+function Feed() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Ian McKean Pinetum</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
     </View>
   );
 }
 
-function App() {
+function Profile() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Ian McKean Pinetum',
-          headerStyle: {
-            backgroundColor: '#1f363d',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>For questions regarding the pinetum contact Alexander McKean at (TODO:THIS)</Text>
+      <Text>If you encounter an error when using this App please send a message to Daniel Murphy danielmurphycs@gmail.com)</Text>
+    </View>
   );
 }
 
-export default App;
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tree Identifier"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Tree Identifier',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="tree" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact Us"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Contact Us',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="phone" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
