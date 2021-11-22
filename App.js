f6464d25954854f45169dfa28ac2755b73a9dbcd
Daniel Murphy <danielmurphycs@gmail.com>
2021-11-22 11:21:00 -0800

    added eslint, formatted App.js

--- App.js
+++ App.js
@@ -1,55 +1,55 @@
 import * as React from 'react';
-import { Text, View, Image, StyleSheet, SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import {
+  Text, View, Image, StyleSheet, SafeAreaView, ScrollView, StatusBar, 
+, Dimensions ,
+  TouchableOpacity,
+
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Dimensions } from 'react-native';
 
-
-import {
-  TouchableOpacity,
-  
-} from "react-native";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-function Feed() {
+const Feed = function () {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#40798c'}}>
-    <View>
-      <Text style={styles.titleText}>Ian McKean Pinetum</Text>
-    </View>
-    <View>
-    <Image
-        style={styles.banner}
-        source={{
+    <View style={{
+    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#40798c' }}>
+      <View>
+        <Text style={styles.titleText}>Ian McKean Pinetum</Text>
+      </View>
+      <View>
+        <Image
+          style={styles.banner}
+          source={{
           uri: 'https://www.irongates.co.nz/uploads/2/4/6/2/24622109/dsc5583_orig.jpg',
-        }}
-      />
+          }}
+        />
+      </View>
+      <Text>
+      Welcome to Ian McKean Pinetum - where you’ll discover the largest collection of conifers in the Southern Hemisphere
+      Founded in 1958, this pinetum is a lifetime of work by farmer and tree-enthusiast Rawhiti “Ian” McKean. The collection includes more than 300 species of conifers, including 90 of the 110 recognised pine, and is planted across 14 hectares of rugged hill-country land on the McKean family farm. Today, it is part of Queen Elizabeth II Trust and open for everyone to enjoy.
+      </Text>
     </View>
-    <Text>Welcome to Ian McKean Pinetum - where you’ll discover the largest collection of conifers in the Southern Hemisphere
-
-Founded in 1958, this pinetum is a lifetime of work by farmer and tree-enthusiast Rawhiti “Ian” McKean. The collection includes more than 300 species of conifers, including 90 of the 110 recognised pine, and is planted across 14 hectares of rugged hill-country land on the McKean family farm. Today, it is part of Queen Elizabeth II Trust and open for everyone to enjoy.</Text>
-    </View>
-    
   );
-}
+};
 
-function Profile() {
+const Profile = function () {
   return (
-    <View style={{flex:1,justifyContent: "center",alignItems: "center"}}>
-    <Text>This App was created in React Native </Text>
-    <Text>To report a problem with the App please contact:</Text>
-    <Text style= {{textDecorationLine: 'underline'}}>danielmurphycs@gmail.com</Text>
-</View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>This App was created in React Native </Text>
+      <Text>To report a problem with the App please contact:</Text>
+      <Text style={{ textDecorationLine: 'underline' }}>danielmurphycs@gmail.com</Text>
+    </View>
   );
-}
+};
 
-function Notifications() {
+const Notifications = function () {
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.scrollView}>
+      <ScrollView style={styles.scrollView}>
       <Text style={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -60,13 +60,13 @@ function Notifications() {
         culpa qui officia deserunt mollit anim id est laborum.
       </Text>
     </ScrollView>
-  </SafeAreaView>
+    </SafeAreaView>
   );
-}
+};
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
+const MyTabs = function () {
   return (
     <Tab.Navigator
       initialRouteName="Feed"
@@ -106,7 +106,7 @@ function MyTabs() {
       />
     </Tab.Navigator>
   );
-}
+};
 
 export default function App() {
   return (
@@ -119,16 +119,16 @@ export default function App() {
 const styles = StyleSheet.create({
   box: {
     width: windowWidth,
-    height: windowHeight/2,
+    height: windowHeight / 2,
   },
   titleText: {
     fontSize: 50,
-    fontWeight: "bold",
-    color:'white',
-    textAlign: "center"
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
   },
   banner: {
     width: windowWidth,
-    height: windowHeight/2,
-  }
+    height: windowHeight / 2,
+  },
 });
