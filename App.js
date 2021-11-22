dcbeec58e72541ed56918d644cb17bf0e5c59cd7
Daniel Murphy <danielmurphycs@gmail.com>
2021-11-22 12:05:48 -0800

    added homepage scrollview

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {
-  Text, View, Image, StyleSheet, SafeAreaView, ScrollView, StatusBar, 
-, Dimensions ,
+  Text, View, Image, StyleSheet, SafeAreaView, ScrollView, StatusBar, Dimensions ,
   TouchableOpacity,
 
 } from 'react-native';
@@ -15,8 +14,8 @@ const windowHeight = Dimensions.get('window').height;
 
 const Feed = function () {
   return (
-    <View style={{
-    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#40798c' }}>
+    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView}>
       <View>
         <Text style={styles.titleText}>Ian McKean Pinetum</Text>
       </View>
@@ -28,11 +27,14 @@ const Feed = function () {
           }}
         />
       </View>
-      <Text>
-      Welcome to Ian McKean Pinetum - where you’ll discover the largest collection of conifers in the Southern Hemisphere
-      Founded in 1958, this pinetum is a lifetime of work by farmer and tree-enthusiast Rawhiti “Ian” McKean. The collection includes more than 300 species of conifers, including 90 of the 110 recognised pine, and is planted across 14 hectares of rugged hill-country land on the McKean family farm. Today, it is part of Queen Elizabeth II Trust and open for everyone to enjoy.
+      <Text style={styles.bodyText}>
+      {`Welcome to Ian McKean Pinetum - where you’ll discover the largest collection of conifers in the Southern Hemisphere.\n\n Founded in 1958, this pinetum is a lifetime of work by farmer and tree-enthusiast Rawhiti “Ian” McKean. The collection includes more than 300 species of conifers, including 90 of the 110 recognised pine, and is planted across 14 hectares of rugged hill-country land on the McKean family farm. Today, it is part of Queen Elizabeth II Trust and open for everyone to enjoy.`}
       </Text>
-    </View>
+ 
+    </ScrollView>
+    </SafeAreaView>
+
+      
   );
 };
 
@@ -48,19 +50,7 @@ const Profile = function () {
 
 const Notifications = function () {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-      <Text style={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-      </Text>
-    </ScrollView>
-    </SafeAreaView>
+    <Text>Under Construction :)</Text>
   );
 };
 
@@ -131,4 +121,15 @@ const styles = StyleSheet.create({
     width: windowWidth,
     height: windowHeight / 2,
   },
+  container: {
+    backgroundColor:'#40798c'
+  },
+  bodyText: {
+    color: 'black',
+    fontSize: 20,
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    margin:10
+  }
 });
